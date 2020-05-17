388fb791355e6d97a23f4e23c9fdd5dacccd82b1
unknown <hofer.ilana@gmail.com>
2020-05-17 21:25:33 +0200

    Completed: Physics Based Character Controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    private float _yVelocity;
+    private bool _canDoubleJump = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get horizontal input
+        // define direction based on that input
+        // MOVE based on that direction
+        CalculateMovement();
+
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        // if grounded
+        // do nothing
+        // else
+        // apply gravity
+        if (_controller.isGrounded == true)
+        {
+            // if I hit the spay key
+            // jump! (assign y velocity to jump height)
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            // check for double jump
+            // current yVelocity += jumpHeight
+            //Debug.Log(_jumpedBefore);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+}
